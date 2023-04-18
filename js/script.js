abe280904f81148a6ba018db01adbde4a401343a
Ana Rosiles <arosiles@hawk.iit.edu>
2023-04-18 18:38:48 -0500

    adding 1/1

--- js/script.js
+++ js/script.js
@@ -39,6 +39,48 @@ async function initMap() {
     center: { lat: 37.0902, lng: -95.7129 },
     zoom: 4.5,
   });
+
+  new google.maps.Marker({
+    position: { lat: 39.5953, lng: -82.5331 },
+    map,
+    title: "John Glenn Columbus International Airport",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 35.4676, lng: -97.5164 },
+    map,
+    title: "Oklahoma City",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 29.1116, lng: -82.0750 },
+    map,
+    title: "Ocala, Florida",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 38.4549, lng: -76.5344 },
+    map,
+    title: "Clinton, Maryland",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 35.5919, lng: -78.5426 },
+    map,
+    title: "Durham, North Carolina",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 41.79225, lng: -87.61719 },
+    map,
+    title: "Washington Park in Chicago",
+  });
+
+  new google.maps.Marker({
+    position: { lat: 40.6023, lng: -75.4714 },
+    map,
+    title: "John Glenn Columbus International Airport",
+  });
 }
 
 initMap();
