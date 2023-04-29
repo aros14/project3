454c628c3804584e4277ccf40f97eca1aabed048
Ana Rosiles <arosiles@hawk.iit.edu>
2023-04-29 17:25:45 -0500

    redoing map

--- js/script.js
+++ js/script.js
@@ -29,3 +29,30 @@ function showSlides(n) {
 }
 
 // map
+
+function init(){
+	alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+	});
+
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', init);
